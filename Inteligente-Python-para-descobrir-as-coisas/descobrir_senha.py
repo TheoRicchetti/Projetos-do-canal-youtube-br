7c047fb615b767b3bef4987e92d09f428f4e2b43
TheoRicchetti <126971605+TheoRicchetti@users.noreply.github.com>
2023-03-04 12:31:35 -0300

    Update descobrir_senha.py

--- Inteligente-Python-para-descobrir-as-coisas/descobrir_senha.py
+++ Inteligente-Python-para-descobrir-as-coisas/descobrir_senha.py
@@ -1,11 +1,7 @@
 from random import*
 acho=""
 senha= input("Digite a senha que vc quer encontrar: ")
-letras=['a','b','c','d','e','f','g','h','i','j',
-        'k','l','m','n','o','p','q','r','s','t',
-        'u','v','w','x','y','z','ç','1','2','3',
-        '4','5','6','7','8','9','0','!','@','#',
-        '$','%','*','(',')','{','}','^','¨','?']
+letras=['1','2','3''4','5','6','7','8','9','0']
 while(acho !=senha):
     acho=""
     for letra in senha:
